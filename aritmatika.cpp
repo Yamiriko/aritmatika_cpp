f19e89251c243cb7462108f0a0f65ee56cfa4643
Jean Riko Kurniawan Putra <52909993+Yamiriko@users.noreply.github.com>
2022-09-30 15:18:09 +0700

    Initial commit

---
+++ aritmatika.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,b,c;
+	cout << "Input Bilangan Pertama : ";
+	cin >> a;
+	cout << "Input Bilangan Kedua : ";
+	cin >> b;
+	c = a + b;
+	cout << "Hasilnya adalah : " << c;
+}
+
